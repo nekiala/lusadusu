9d2700078cdc5392999d955e0ffd1c11fbd27c6a
Kiala Ntona <nekiala@gmail.com>
2020-02-25 14:17:20 +0100

    added affiliates migration

---
+++ database/migrations/2020_02_25_131211_create_affiliates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAffiliatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('affiliates', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('affiliate_parameter_id');
+            $table->unsignedBigInteger('user_id')->unique();
+            $table->boolean('status')->default(false);
+            $table->text('comments')->nullable()->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('affiliates');
+    }
+}
