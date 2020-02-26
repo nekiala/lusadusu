77ac3c045a9a807b9ed9ba4331c256af79657bee
Kiala Ntona <nekiala@gmail.com>
2020-02-26 15:17:57 +0100

    added Category seeder

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Category::truncate();
+
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 20; $i ++) {
+
+            \App\Category::create([
+                'name' => $faker->unique()->city
+            ]);
+        }
+    }
+}
